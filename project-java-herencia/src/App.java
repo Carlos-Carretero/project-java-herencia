ed6652042aa1af91b73a06a5bdfe8b06af197f99
Ccarretero <carreteroestudios@gmail.com>
2025-05-27 20:14:42 -0500

    Creacion de carpeta models, importacion en app.java y creacion de objeto

--- project-java-herencia/src/App.java
+++ project-java-herencia/src/App.java
@@ -1,5 +1,8 @@
+import models.ComponenteVehicular;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    ComponenteVehicular myComponenteVehicular = new ComponenteVehicular("L0L", "Toyota");
 }
